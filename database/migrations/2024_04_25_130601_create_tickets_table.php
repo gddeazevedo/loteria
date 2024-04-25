ecde638b10ba5eadbeebc0bfb04425215bc34e62
Gabriel D. Azevedo <42484610+gddeazevedo@users.noreply.github.com>
2024-04-25 10:13:31 -0300

    creating create_tickets_table migration

---
+++ database/migrations/2024_04_25_130601_create_tickets_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->string('code', length: 14)->nullable(false);
+            $table->string('name', 50)->nullable(false);
+            $table->json('numbers')->nullable(false);
+            $table->json('machine_numbers')->nullable(false);
+            $table->primary('code');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
