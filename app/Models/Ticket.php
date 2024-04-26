ede94a965f09a2f2e076985693b472c08b1e3458
Gabriel D. Azevedo <42484610+gddeazevedo@users.noreply.github.com>
2024-04-25 21:17:05 -0300

    creating ticket model

---
+++ app/Models/Ticket.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Ticket extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'code';
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $fillable = ['code', 'name', 'numbers', 'machine_numbers'];
+
+    // protected function numbers(): Attribute
+    // {
+    //     return Attribute::make(
+    //         get: fn (string $val) => json_decode($val, true),
+    //         set: fn(array $val) => json_encode($val)
+    //     );
+    // }
+
+    // protected function machine_numbers(): Attribute
+    // {
+    //     return Attribute::make(
+    //         get: fn (string $val) => json_decode($val, true),
+    //         set: fn(array $val) => json_encode($val)
+    //     );
+    // }
+}
