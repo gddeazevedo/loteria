5144da7dfd534c53ac6167c658eb18245d8397a9
Gabriel D. Azevedo <42484610+gddeazevedo@users.noreply.github.com>
2024-04-25 21:44:58 -0300

    implementing ticketcontroller

---
+++ app/Http/Controllers/TicketController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Ticket;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class TicketController extends Controller
+{
+    public function store(Request $request): JsonResponse
+    {
+        $rules = [
+            'name' => ['required', 'max:50'],
+            'numbers' => ['required', 'array', 'min:6', 'max:6']
+        ];
+
+        $validated = $request->validate($rules);
+        $ticket_code = '';
+        $ticket = Ticket::create([
+            'code' => $ticket_code,
+            ...$validated
+        ]);
+
+        // PrizeDrawJob::dispatch($ticket);
+
+        return response()->json(['ticketCode' => $ticket_code], 201);
+    }
+
+    public function show(string $code): JsonResponse
+    {
+        $ticket = Ticket::find($code);
+        $machine_numbers = json_decode($ticket->machine_numbers, true);
+
+        $res = [
+            'name'=> $ticket->name,
+            'yourNumbers' => $ticket->numbers,
+            'machineNumbers' => $machine_numbers,
+            'winner' => $ticket->numbers === $machine_numbers,
+        ];
+
+        if ($machine_numbers === null) {
+            $res['message'] = 'not yet';
+            return response()->json($res);
+        }
+
+        $res['message'] = $res['winner'] ? 'you win' : 'you lost';
+
+        return response()->json($res);
+    }
+}
